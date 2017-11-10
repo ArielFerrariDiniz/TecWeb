b39057838d9c96a01de03bac562b2b3a090606d6
ArielFerrariDiniz <ariel_ferrari_diniz@hotmail.com>
2017-11-09 22:11:25 -0200

    Inicio

---
+++ TecWebProject/TecWebProject/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(TecWebProject.Startup))]
+namespace TecWebProject
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
