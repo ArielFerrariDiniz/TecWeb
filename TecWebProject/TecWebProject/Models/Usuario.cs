25233af756fea37f6a2a6183ff03c629003ed065
ArielFerrariDiniz <ariel_ferrari_diniz@hotmail.com>
2017-11-09 22:25:39 -0200

    Atributos em Usuario

--- TecWebProject/TecWebProject/Models/Usuario.cs
+++ TecWebProject/TecWebProject/Models/Usuario.cs
@@ -7,5 +7,8 @@ namespace TecWebProject.Models
 {
     public class Usuario
     {
+        public int Id { get; set; }
+        public string Nome { get; set; }
+        public string Senha { get; set; }
     }
 }
